4805cee59a1af5c3870064b22323af57a317cc69
Jimmy Ngo <jngo2013@gmail.com>
2020-07-29 15:25:59 -0700

    fix About page

--- src/component/About/index.js
+++ src/component/About/index.js
@@ -12,7 +12,7 @@ function About(){
         <p className="col-9">Full Stack Web Developer with a background in customer service and management. I recently earned my certificate in full-stack web development from the University of California, Berkeley Extension and am experienced in MERN stack (MongoDB, Express.js, React.js, and Node.js),HTML, CSS, and JavaScript. I managed a boba shop at the University of California, Berkeley campus for three years. During this time, I was able to gain more experience in managing multiple priorities, working with a team, making decisions, and communicating effectively. I bring a focus on user experience of websites and learning new frameworks for them.
         </p>
         <p>
-        Prior to earning my certificate in Full-stack web development, I managed a boba shop at the University of California, Berkeley campus for three years. During this time, I was able to gain more experience in managing multiple priorities, working with a team, making decisions, and communicating effectively. Being a manager has allowed me to constantly grow in many skills such as managing employees, communicating effectively, and solving problems efficiently. I aim to utilize every skill that I have obtained from my previous fields to be of value to the web development community.</p>
+        Being a manager has allowed me to constantly grow in many skills such as managing employees, communicating effectively, and solving problems efficiently. I aim to utilize every skill that I have obtained from my previous fields to be of value to the web development community.</p>
       </div>
     </div>
   </div>
