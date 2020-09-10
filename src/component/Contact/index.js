3aea24ad722a47eaa880754b609ce4d32dc06c10
Jimmy Ngo <jngo2013@gmail.com>
2020-09-10 14:47:20 -0700

    add linkedIn and Github to Contact

--- src/component/Contact/index.js
+++ src/component/Contact/index.js
@@ -7,14 +7,16 @@ function Contact() {
         <div className="col-lg-3 col-md-6 col-sm-12">
           <section>
             <h1 className="display-4"> Contact</h1>
+              <div></div><a href="https://github.com/jngo2013">Github</a>
             <form>
-              <label for="name" style={{marginTop: '20px'}}>Name</label>
+              <a href="https://www.linkedin.com/in/jimmyngo95/" target="">Linkedin</a>
+              {/* <label for="name" style={{marginTop: '20px'}}>Name</label>
               <input className="form-control" />
               <label for="email" style={{marginTop: '20px'}}>Email</label>
               <input className="form-control" />
               <label for="message" style={{marginTop: '20px'}}>Message</label>
               <textarea className="form-control" rows="5"></textarea>
-              <button style={{marginTop: '20px'}} className="btn btn-primary">Submit</button>
+              <button style={{marginTop: '20px'}} className="btn btn-primary">Submit</button> */}
             </form>
           </section>
         </div>
